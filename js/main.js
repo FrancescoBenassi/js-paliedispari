f10ae6f24f0b3659e93d9e7f24732b6ba370b89d
FrancescoBenassi <francybenna77@gmail.com>
2024-10-18 16:25:44 +0200

    feat: added const

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// # Palindroma
+
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+// 1) Chiedere all'utente una parola usando prompt()
+
+const word = prompt();
+console.log(word);
+
+// 2) Creare una funzione per capire se la parola inserita è palindroma
+//     - Creare una inversione di parola
+//     - Confrontare le 2 parola (quella originale e quella inversa)
+//     - Se corrispondono la parola è palindorma altrimenti non lo è
+
+// # Pari e Dispari
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
