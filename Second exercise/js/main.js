c8df42d2fcfbed46fdd837600373e3a3942b7414
FrancescoBenassi <francybenna77@gmail.com>
2024-10-18 17:28:11 +0200

    feat: added second exercise

---
+++ Second exercise/js/main.js
@@ -0,0 +1,19 @@
+// /# Pari e Dispari
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// 1) Chiedere all'utente pari o dispari con prompt
+
+// 2) Chiedere all'utente un numero da 1 a 5
+
+// 3) Creiamo una variabile con il risultato della funzione e decreta se hai vinto o perso
+
+// 4) Creiamo una funzione per validare se l'utente ha perso o no contro il computer
+//     - Creiamo una variabile con un numero random
+//     - Sommare il numero dell'utente con quello del computer
+//     - Validare se è un numero pari o dispari
+//     - In base a cosa ha scelto l'utente stampare se l'utente ha vinto o perso 
